7b4c7a3daeb63238ccd4c3ba554f9f9ffc8e9863
Valentin Kormanov <guve4e@gmail.com>
2018-10-14 14:26:47 -0500

    Adds new File class

---
+++ src/file.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import fileinput
+
+
+class File(object):
+
+    def __init__(self, file_name: str)-> None:
+        super().__init__()
+
+        self.__file_name = file_name
+
+    def __add_line(self):
+        pass
+
+    def add(self):
+        pass
+
+    def change(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def append(self, text: str):
+        pass
+
+    def prepend(self):
+        pass
+
---
+++ unittests/test_file.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+import unittest
+from src.file import File
+
+class FileTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def runTest(self):
+        pass
+
+    def tearDown(self):
+       pass
+
+    def test_append(self):
+        # Arrange
+        file = File("testfile.json")
+
+        # # Act
+
+        # # Assert
+        pass
+
+    def test_preppend(self):
+        # Arrange
+        file = File("testfile.json")
+
+        # # Act
+
+        # # Assert
+        pass
+
+    def test_add(self):
+        # Arrange
+        file = File("testfile.json")
+
+        # # Act
+
+        # # Assert
+        pass
+
+    def test_change(self):
+        # Arrange
+        file = File("testfile.json")
+
+        # # Act
+
+        # # Assert
+        pass
+
+    def test_remove(self):
+        # Arrange
+        file = File("testfile.json")
+
+        # # Act
+
+        # # Assert
+        pass
